20c934094688d7ccbabb24b101caa9ee0a4f814d
asd123www <352178414@qq.com>
2026-03-22 23:51:10 +0000

    remove mha

--- nanogpt/config.py
+++ nanogpt/config.py
@@ -3,7 +3,7 @@ import jax.numpy as jnp
 import yaml
 import dataclasses
 from pathlib import Path
-from typing import Callable, Tuple, Optional
+from typing import Callable, Optional, Tuple
 from jax.sharding import Mesh
 from utils import jax_pytree_struct
 
@@ -40,32 +40,6 @@ class EmbeddingConfig:
 
     def __post_init__(self):
         self.weight_initializer = jax.nn.initializers.normal(stddev=1.0)
-
-
-@dataclasses.dataclass
-class MultiHeadAttentionConfig:
-    dtype: jnp.dtype = jnp.bfloat16
-    d_in: int = 768
-    d_out: int = 768
-    num_heads: int = 12
-    num_layers: int = 12
-
-    wq_initializer: Callable = dataclasses.field(init=False)
-    wk_initializer: Callable = dataclasses.field(init=False)
-    wv_initializer: Callable = dataclasses.field(init=False)
-    wo_initializer: Callable = dataclasses.field(init=False)
-
-    wq_logical_axes: Tuple[str, str] = ("attn_wq_in", "attn_wq_out")
-    wk_logical_axes: Tuple[str, str] = ("attn_wk_in", "attn_wk_out")
-    wv_logical_axes: Tuple[str, str] = ("attn_wv_in", "attn_wv_out")
-    wo_logical_axes: Tuple[str, str] = ("attn_wo_in", "attn_wo_out")
-
-    def __post_init__(self):
-        init = init_uniform(scale=3**0.5 * self.d_emb**-0.5)
-        self.wq_initializer = init
-        self.wk_initializer = init
-        self.wv_initializer = init
-        self.wo_initializer = jax.nn.initializers.zeros
 
 
 @dataclasses.dataclass
@@ -137,52 +111,28 @@ class ModelConfig:
     num_layers: int = 16
     q_heads: int = 8
     kv_heads: int = 4
-    attn_type: str = "gqa"
     attn_impl: str = "flash_attn"
-    num_heads: Optional[Tuple[int, None]] = dataclasses.field(init=False)
     dtype: jnp.dtype = jnp.bfloat16
 
     embed: EmbeddingConfig = dataclasses.field(init=False)
     mlp: MLPConfig = dataclasses.field(init=False)
     lm_head: LinearConfig = dataclasses.field(init=False)
-
-    if attn_type == "mha":
-        attn: MultiHeadAttentionConfig = dataclasses.field(init=False)
-    elif attn_type == "gqa":
-        attn: GroupedQueryAttentionConfig = dataclasses.field(init=False)
-    else:
-        raise ValueError(
-            f"Only these attention types are supported for now `['gqa', 'mha']`. Received = {attn_type}"
-        )
+    attn: GroupedQueryAttentionConfig = dataclasses.field(init=False)
 
     def __post_init__(self):
-        if self.q_heads == self.kv_heads and self.attn_type == "gqa":
-            raise Warning(
-                "When the number of query heads equals the number of kv heads, JAX computes MHA not GQA!"
-            )
-
         self.embed = EmbeddingConfig(
             dtype=self.dtype,
             vocab_size=self.vocab_size,
             d_emb=self.d_emb,
             num_layers=self.num_layers,
         )
-        if self.attn_type == "mha":
-            self.attn = MultiHeadAttentionConfig(
-                dtype=self.dtype,
-                d_in=self.d_emb,
-                d_out=self.d_emb,
-                num_heads=self.num_heads,
-                num_layers=self.num_layers,
-            )
-        elif self.attn_type == "gqa":
-            self.attn = GroupedQueryAttentionConfig(
-                dtype=self.dtype,
-                d_emb=self.d_emb,
-                q_heads=self.q_heads,
-                kv_heads=self.kv_heads,
-                num_layers=self.num_layers,
-            )
+        self.attn = GroupedQueryAttentionConfig(
+            dtype=self.dtype,
+            d_emb=self.d_emb,
+            q_heads=self.q_heads,
+            kv_heads=self.kv_heads,
+            num_layers=self.num_layers,
+        )
         self.mlp = MLPConfig(dtype=self.dtype, d_emb=self.d_emb)
         self.lm_head = LinearConfig(
             dtype=self.dtype,
